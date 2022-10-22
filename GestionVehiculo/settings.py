2efe177a2a7f3ccf22623748a92eb78e5459dd2e
arangojimmy535 <39570437+arabgojimmy535@users.noreply.github.com>
2022-10-21 21:12:30 -0500

    Update settings.py

--- GestionVehiculo/settings.py
+++ GestionVehiculo/settings.py
@@ -169,8 +169,4 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'media')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
