3ef65cab7d58f563b1eecc8a902bf98f54495471
arangojimmy535 <39570437+arabgojimmy535@users.noreply.github.com>
2022-10-22 12:32:33 -0500

    Update api.py

--- Vehiculos/api.py
+++ Vehiculos/api.py
@@ -32,7 +32,7 @@ class LoginAPI(KnoxLoginView):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         login(request, user)
-        return Response({"user": UserSerializer(user, context=self.get_serializer_context()).data,}) ,super(LoginAPI, self).post(request, format=None)
+        return super(LoginAPI, self).post(request, format=None)
 
 
 
