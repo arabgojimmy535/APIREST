8bd904b79ffabaed9db0f1e82775cbbe96b4f3c8
arangojimmy535 <39570437+arabgojimmy535@users.noreply.github.com>
2022-10-21 21:17:02 -0500

    Update settings.py

--- GestionVehiculo/settings.py
+++ GestionVehiculo/settings.py
@@ -157,16 +157,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    BASE_DIR/'static'
-] 
-MEDIA_URL= '/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
-
+STATIC_URL = '/static/'
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
