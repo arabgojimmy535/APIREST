a3ae5edb61c3ffd8ffdd01accbf2a3e933accebf
arangojimmy535 <39570437+arabgojimmy535@users.noreply.github.com>
2022-10-22 09:33:17 -0500

    Update models.py

--- Vehiculos/models.py
+++ Vehiculos/models.py
@@ -28,6 +28,7 @@ class vehiculos(models.Model):
     usado= models.BooleanField(default=False)
     imagen= models.URLField()
     vendido= models.BooleanField(default=False,null=True)
+    creadopor= models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.marca} {self.modelo} {self.año}"
