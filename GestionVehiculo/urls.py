460deba253695ce9cf92750e31529e49855937bb
arangojimmy535 <39570437+arabgojimmy535@users.noreply.github.com>
2022-10-21 21:06:17 -0500

    Update urls.py

--- GestionVehiculo/urls.py
+++ GestionVehiculo/urls.py
@@ -35,6 +35,3 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
-if settings.DEBUG :
-    urlpatterns+=static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT )
-    urlpatterns+=static(settings.STATIC_URL , document_root = settings.STATIC_ROOT )
